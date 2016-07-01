286180febef1db9c1f83825553ecddcbf2d94e31
vic511 <vic511@hotmail.fr>
2016-07-01 03:10:29 +0200

    Shellcode wrapper enhancement

--- exec.c
+++ exec.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/mman.h>
+#include <libgen.h>
 
 int main(int argc, char *argv[]){
 
@@ -11,20 +12,20 @@ int main(int argc, char *argv[]){
     }
 
     FILE* fd = fopen(argv[1], "r");
-    long size;
-
     if(fd == NULL)
     {
         fprintf(stderr, "Error: unable to open %s\n", argv[1]);
         return EXIT_FAILURE;
     }
 
+    long size;
     fseek(fd, 0, SEEK_END);
     size = ftell(fd);
     fseek(fd, 0, SEEK_SET);
 
-    void* addr = mmap(NULL, (size_t) size, PROT_EXEC | PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+    fprintf(stderr, "[+] %s - %ld bytes\n", basename(argv[1]), size);
 
+    void* addr = mmap(NULL, (size_t) size, PROT_EXEC | PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
     if(addr == MAP_FAILED)
     {
         fprintf(stderr, "Error: mmap\n");
@@ -32,13 +33,13 @@ int main(int argc, char *argv[]){
     }
 
     int i = 0;
-
     while(!feof(fd))
     {
         *(char*) (addr + i) = fgetc(fd);
         i++;
     }
 
+    fprintf(stderr, "[+] Executing...\n");
     (*(void(*)()) addr)();
 
     fclose(fd);
